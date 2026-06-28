7a7d79c70be0572ab4494d6e9ca14cd090014c02
Ronald Nieuwenhuis <ronald.nieuwenhuis@iquality.nl>
2026-06-28 10:22:36 +0200

    Fix Storyblok index signature compatibility

--- src/components/loan-calculator/LoanCalculator.tsx
+++ src/components/loan-calculator/LoanCalculator.tsx
@@ -10,7 +10,7 @@ export interface LoanPurpose {
 }
 
 export interface LoanCalculatorStoryblok {
-  [key: string]: unknown
+  [key: string]: any
   _uid: string
   _editable?: string
   component: 'loan_calculator'
